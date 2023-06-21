97c45c581a65ea70fe7684f82ab6de8b75802e87
MorganNdungu <ndungumorgan20@gmail.com>
2023-06-21 11:34:34 +0300

    3rd

---
+++ routes/api.js
@@ -0,0 +1,27 @@
+const express=require ('express');
+const routes=express.Router();
+
+//get alist of student from the database
+routes.get ('/student', (req, res)=>{
+    res.send({type:'get Request'});
+});
+
+//add student to the db
+routes.post('/students',(req, res)=>{
+        res.send({type:'post Request'});
+
+});
+
+//update
+routes.put('/students',(req, res)=>{
+        res.send({type:'update Request'});
+
+});
+
+//delete
+routes.delete('/students',(req, res)=>{
+        res.send({type:'delete Request'});
+
+});
+
+module.exports=routes;
