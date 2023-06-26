a08e44775b165866fe868de43184bf67e46d8809
MorganNdungu <ndungumorgan20@gmail.com>
2023-06-26 11:45:32 +0300

    5th

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express=require('express');
 const routes=require('./routes/api');
 const app=express();
+
+const studentRoutes = require('./routes/api')
+
+app.use('/students', studentRoutes);
+
 app.use(routes);
+
+
+//handling 404
+app.use((err, req, res, next)=>{
+    res.status(err.status || 500)
+    res.send({
+      error:{
+        status: err.status || 500,
+        message: err.message
+      };
+    })
+})
+
 app.listen(process.env.port || 4000, function(){
     console.log('now listening for request on:http://localhost:4000');
 });
---
+++ routes/studentapi.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const routes=require('./routes/studentapi.js');
+const app=express();
+
+app.use('/students', studentsRoute);
+
+app.use(routes);
+
+//last code
+//port
+app.listen(process.env.port || 4000, function(){
+    console.log('now listening for request on:http://localhost:4000');
+});
